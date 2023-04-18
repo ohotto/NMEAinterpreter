07c458b73af7e649a2fa53a659ea2eeec3cfacce
OttoLi <ottoli.pro@gmail.com>
2023-04-18 22:17:40 +0800

    v0.2

--- src/NMEAi.cpp
+++ src/NMEAi.cpp
@@ -1,7 +1,7 @@
 /**
  * @author OttoLi (ottoli.pro@gmail.com)
- * @brief 从串口接收NMEA数据，并输出
- * @version 0.1
+ * @brief 从串口接收NMEA数据，并筛选，只输出$GPGGA、$GPRMC和$WTRTK
+ * @version 0.2
  * @date 2023-04-17
  * 
  * @copyright Copyright (c) 2023
@@ -9,9 +9,29 @@
  */
 
 #include <iostream>
+#include <cstring>
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
+
+const std::string NMEA_GGA = "$GPGGA";
+const std::string NMEA_RMC = "$GPRMC";
+const std::string NMEA_WTRTK = "$WTRTK";
+
+bool IsNmeaValid(const std::string& nmea)
+{
+    if (nmea.empty() || nmea[nmea.size() - 1] != '\n') {
+        return false;
+    }
+
+    if (nmea.compare(0, NMEA_GGA.size(), NMEA_GGA) != 0
+        && nmea.compare(0, NMEA_RMC.size(), NMEA_RMC) != 0
+        && nmea.compare(0, NMEA_WTRTK.size(), NMEA_WTRTK) != 0) {
+        return false;
+    }
+
+    return true;
+}
 
 int main()
 {
@@ -33,10 +53,19 @@ int main()
 
     // 读取NMEA数据并打印到控制台窗口中
     char szBuff[1024] = { 0 };
+    std::string nmea;
     while (true) {
         ssize_t nRead = read(fd, szBuff, sizeof(szBuff));
         if (nRead > 0) {
-            std::cout << szBuff;
+            nmea.append(szBuff, nRead);
+            size_t pos = 0;
+            while ((pos = nmea.find('\n')) != std::string::npos) {
+                std::string sentence = nmea.substr(0, pos + 1);
+                if (IsNmeaValid(sentence)) {
+                    std::cout << sentence;
+                }
+                nmea.erase(0, pos + 1);
+            }
         }
     }
 
